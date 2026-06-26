de1d663e791f75a60378df021580f0a572900bc6
Sandra Blanco <sandra.blanco18@gmail.com>
2026-06-26 10:26:50 +0200

    for loop

--- static/script.js
+++ static/script.js
@@ -5,3 +5,28 @@ fetch("/tablero")
   });
   document.getElementById("history").innerHTML = "Bienvenido a tu primer caso de murdoku. Resuelve el misterio completando el tablero."=data.history;
   let tablero=document.getElementById("tablero");
+  
+  for (let i = 0; i < data.casillas.length; i++) {
+    let c = data.casillas[i];
+    let div = document.createElement("div");
+    div.className = "casilla";
+    let texto =
+        "F: " + c.fila +
+        " C: " + c.columna +
+        "\n" +
+        c.contenido;
+    div.innerText = texto;
+    if (c.fila <= 3 && c.columna <= 3) {
+        div.classList.add("sala");
+    }
+    else if (c.fila <= 3 && c.columna >= 4) {
+        div.classList.add("dormitorio");
+    }
+    else if (c.fila >= 4 && c.columna <= 3) {
+        div.classList.add("salon");
+    }
+    else {
+        div.classList.add("comedor");
+    }
+    tablero.appendChild(div);
+}
